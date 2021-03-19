ae56bbfa807a4b52702a206e1421b0dffa7d8a17
Julián Lopera <julian.lopera.ext@kavak.com>
2021-03-19 13:22:43 -0500

    cofig: production envs

--- config/production.js
+++ config/production.js
@@ -2,7 +2,14 @@ exports.config = {
   environment: 'production',
   common: {
     database: {
-      name: process.env.DB_NAME
+      name: process.env.DB_NAME_DEV
+    },
+    witter_api: {
+      url: process.env.WITTER_API_URL
+    },
+    session: {
+      header_name: 'authorization',
+      secret: process.env.NODE_API_SESSION_SECRET
     }
   },
   isProduction: true
