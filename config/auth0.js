480f3448e323de668ce93243e107c9d390502968
Julián Lopera <julian.lopera.ext@kavak.com>
2021-04-06 06:32:23 -0500

    feat: initial config to auth0

---
+++ config/auth0.js
@@ -0,0 +1,12 @@
+const { auth } = require('express-openid-connect');
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: 'a long, randomly-generated string stored in env',
+  baseURL: process.env.BASE_URL || `https://localhost:${process.env.PORT}`,
+  clientID: 'tj46FnmL19wYzZovONham9xtKFyZuHc0',
+  issuerBaseURL: 'https://dev-fmtgdiik.us.auth0.com'
+};
+
+exports.init = app => app.use(auth(config));
